80411732c3ef1c63c3f102d04becb7e4fe468ca5
“juanP1996” <parrajp1996@gmail.com>
2022-02-22 15:52:59 +0100

    estructura creada con sus endpooints, conroladores, algunos controles de seguridades y conectada a su ddbb

---
+++ models/pelicula.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pelicula extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasMany(models.Order, {
+        foreignKey: 'peliculaId'
+      });
+    }
+  }
+  Pelicula.init({
+    titulo: DataTypes.STRING,
+    sinopsis: DataTypes.STRING,
+    adult: DataTypes.BOOLEAN,
+    popularity: DataTypes.FLOAT,
+    image: DataTypes.STRING,
+    fecha: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Pelicula',
+  });
+  return Pelicula;
+};
