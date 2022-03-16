efbe152050b1bacc2aaf0ed446d5c7e875e94ca7
“juanP1996” <parrajp1996@gmail.com>
2022-03-16 20:42:59 +0100

    actualizo4

--- views/PeliculasRouter.js
+++ views/PeliculasRouter.js
@@ -12,7 +12,7 @@ const PeliculasController = require('../controllers/PeliculasController');
 
 router.get('/importadas', PeliculasController.importaPeliculas);
 
-router.post('/importadas2', PeliculasController.importaPeliculas2);
+router.get('/importadas2', PeliculasController.importaPeliculas2);
 
 router.get('/todas', PeliculasController.traePelicula);
 
