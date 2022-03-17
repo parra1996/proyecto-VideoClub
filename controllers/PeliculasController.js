ab80f4cce446c28222bc803d584ce34fcfc40a67
“juanP1996” <parrajp1996@gmail.com>
2022-03-17 09:43:20 +0100

    arreglandomendpoint de importa peliculas

--- controllers/PeliculasController.js
+++ controllers/PeliculasController.js
@@ -27,32 +27,29 @@ PeliculasController.importaPeliculas2 = async () => {
     const minMaxRoundedRandom = (min, max) => {
         return Math.round(Math.random() * (max - min) + min);
     }
-    clone = async () => {
         ///Variable para guardar el root para ver el póster
         let TMDBimgUrlRoot = "https://image.tmdb.org/t/p/original";
         //Endpoint para traerme una página entera de películas. Necesario para tenerlo una primera vez
         let firstScan = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`)
         //bucle para recorrer 25 páginas de resultados. El valor de page lo saco de una función random para que no siempre muestre las mismas páginas.
-        for(let j=1 ; j<=25 ; j++) {
+        for(let j=1 ; j<=10 ; j++) {
             let resultss = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${minMaxRoundedRandom(1, 25)}&with_watch_monetization_types=flatrate`);
             //Saco el número de resultados por página para meterselo al siguiente bucle
             let numbOfResultsPerPageTMDB = resultss.data.results.length
             //Recorro cada elemento de la página para ir guardándolo acorde a los campos de mi BBDD
             for(let i=0; i<numbOfResultsPerPageTMDB ; i++) {
                 //Por cada iteración creo un elemento
-                Film.create({
+                Pelicula.create({
                     //A la izquierda mis campos de mi BBDD
                     //A la derecha los campos que devuelve TMDB
                     titulo : resultss.data.results[i].original_title,
                     sinopsis : resultss.data.results[i].overview,
-                    fecha : resultss.data.results[i].date,
-                    popularity : resultss.data.results[i].popularity,
+                    adult : resultss.data.results[i].adult,
                     image : (TMDBimgUrlRoot + "/" + resultss.data.results[i].poster_path)
                 })
             }
         }
         return (`${25} pages have been clonated succesfully, with a total amount of ${500} films`)
-    };
 
     };
 
